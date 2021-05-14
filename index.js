486dce599eb21247343e3416e7a095305fd3b37d
rilith <bludoftenchiefs@protonmail.com>
2021-05-13 19:07:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { useFakeXMLHttpRequest } = require("sinon");
+
 // Code your solution here
+function findMatching(drivers, name) {
+    let array = drivers.filter(function (driver){
+        return driver.toLowerCase() === name.toLowerCase();
+     })
+     console.log(array);
+     return array;
+}
+
+function fuzzyMatch (drivers, letter) {
+    let arrays = drivers.filter(function (driver){
+        return driver.startsWith(letter);
+    })
+    console.log(arrays);
+    return arrays;
+}
+
+function matchName (drivers, title) {
+    let arrays = drivers.filter(function (driver){
+        return driver.name === title;
+    })
+    console.log(arrays);
+    return arrays;
+}
+
